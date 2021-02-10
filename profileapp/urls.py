fd17fb9423348d301823ca134f5e54b508758777
joohong <joohong2000@gmail.com>
2021-02-11 01:05:42 +0900

    commit

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from profileapp.views import ProfileCreateView
 
+from profileapp.views import ProfileUpdateView
+
 app_name = 'profileapp'
 
 urlpatterns =[
     path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.models import Profile
 
 from profileapp.forms import ProfileCreationForm
+
+from profileapp.decorators import profile_ownership_required
 
 
 class ProfileCreateView(CreateView):
@@ -22,3 +25,13 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
 
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
