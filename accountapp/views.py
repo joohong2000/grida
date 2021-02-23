989077bfc199ee9d07ec0fa50b0611a036c5dc90
joohong <joohong2000@gmail.com>
2021-02-23 23:57:05 +0900

    commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
@@ -14,26 +13,6 @@ from accountapp.forms import AccountUpdateForm
 from django.views.generic.list import MultipleObjectMixin
 
 from articleapp.models import Article
-
-
-def hello_world(request):
-
-    if request.user.is_authenticated:
-        if request.method == "POST":
-
-            temp = request.POST.get('hello_world_input')
-
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
